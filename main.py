f7497778a0d3d88ed48160347ea015361e89005e
jackwillson642 <jackwillson642@gmail.com>
2021-07-01 15:21:35 +0530

    Only added/modified files are copied to the restricted direcotry

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 import shutil
 import os
+from filecmp import dircmp
 
-all_dirs_list = os.listdir(".")
+
+all_dirs_list = os.listdir(".")  # list of all directories in the directory that this script is in
+
+# Declaring and initializing a list containing all Dev directories
 dev_dirs_list = []
-
 for i in all_dirs_list:
     if (i[0] == "D") and (i[1] == "e") and (i[2] == "v"):
         dev_dirs_list.append(i)
-        print(i)
 
-for i in dev_dirs_list:
-    src_path = "./" + i
-    dest_path = "./restricted/" + i
-    shutil.copytree(src_path, dest_path, copy_function=shutil.copy, dirs_exist_ok=True)
+
+def copy_tree(dir1, dir2):
+
+    comparison = dircmp(dir1, dir2)
+    for i in comparison.left_only:
+        src = dir1 +"/"+ i
+        dest = dir2 +"/"+ i
+        if(os.path.isdir(src)):
+            shutil.copytree(src, dest, dirs_exist_ok=True)
+            print(src+" Copied to "+dest)
+        else:
+            shutil.copy(src, dest)
+            print(src+" copied To "+dest)
+
+    for i in comparison.diff_files:
+        src = dir1 +"/"+ i
+        dest = dir2 
+        shutil.copy(src, dest)
+        print(src+"copied to"+dest)
+
+    for i in comparison.common_dirs:
+        new_dir1 = dir1 + "/" + i
+        new_dir2 = dir2 + "/" + i
+        copy_tree(new_dir1, new_dir2)
+
+for dev_dir in dev_dirs_list:
+    src_path = "./" + dev_dir
+    dest_path = "./AllDev/" + dev_dir
+    copy_tree(src_path, dest_path)
